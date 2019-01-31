daf633bd5eb2a2845a02768b14a7d36e29f85f1c
Feross Aboukhadijeh <feross@feross.org>
2019-01-31 14:07:35 -0800

    Add ostreamlock.cc/.h

---
+++ src/ostreamlock.h
@@ -0,0 +1,32 @@
+#ifndef _RAFT_OSTREAMLOCK_H_
+#define _RAFT_OSTREAMLOCK_H_
+
+/**
+ * Implementation from CS110
+ * /usr/class/cs110/local/include/ostreamlock.h
+ */
+
+#include <ostream>
+#include <iostream>
+#include <mutex>
+#include <memory>
+#include <map>
+
+/**
+ * Two stream manipulators designed to make insertion into ostreams thread-safe.
+ *
+ * Provides a locking mechanism to lock down access to cout, cerr, and other
+ * stream references so that the full accumulation of daisy-chained data is
+ * inserted into an ostream as one big insertion.
+ *
+ * Examples:
+ *     Thread safe:
+ *         cout << oslock << 1 << 2 << 3 << 4 << endl << osunlock;
+ *
+ *     Not thread safe:
+ *         cout << 1 << 2 << 3 << 4 << endl;
+ */
+std::ostream& oslock(std::ostream& os);
+std::ostream& osunlock(std::ostream& os);
+
+#endif
