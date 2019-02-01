c9fc5e0d23f7d242e606d96eb9166223afafb3da
Feross Aboukhadijeh <feross@feross.org>
2019-01-31 16:01:57 -0800

    more readable protocol buffer logs

--- src/util.cc
+++ src/util.cc
@@ -5,7 +5,7 @@ const string Util::ProtoDebugString(::google::protobuf::Message& message) {
     // Remove trailing newlinem
     str = str.substr(0, str.size() - 1);
     // Replace newlines with commas for a more compact representation
-    replace(str.begin(), str.end(), '\n', ',');
+    replace(str.begin(), str.end(), '\n', ' ');
     return  str;
 }
 
